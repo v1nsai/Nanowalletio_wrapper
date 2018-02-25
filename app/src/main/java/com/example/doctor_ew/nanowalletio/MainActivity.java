d1ddeb5a1a257ee1377449b2b4b536c72a380129
Drew Riffle <andrewlriffle@protonmail.com>
2018-02-25 10:22:37 -0500

    Initial commit

---
+++ app/src/main/java/com/example/doctor_ew/nanowalletio/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.doctor_ew.nanowalletio;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // remove title
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        WebView webview = (WebView)findViewById(R.id.nanowallet);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl("https://www.nanowallet.io");
+
+        webview.setWebViewClient(new WebViewClient() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return false;
+            }
+        });
+    }
+}
