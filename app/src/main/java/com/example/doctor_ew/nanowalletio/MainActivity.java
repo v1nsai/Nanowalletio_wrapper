d489db8bbcea5b10dd4ac21b1986aee989c3654d
Drew Riffle <andrewlriffle@protonmail.com>
2018-02-25 10:31:08 -0500

    Minor fixes

--- app/src/main/java/com/example/doctor_ew/nanowalletio/MainActivity.java
+++ app/src/main/java/com/example/doctor_ew/nanowalletio/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.doctor_ew.nanowalletio;
 
 import android.os.Build;
 import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -14,24 +17,33 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // remove title
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // remove status bar and make fullscreen
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        //Hide the giant pointless title bar
         getSupportActionBar().hide();
 
-        WebView webview = (WebView)findViewById(R.id.nanowallet);
-        webview.getSettings().setJavaScriptEnabled(true);
-        webview.loadUrl("https://www.nanowallet.io");
+        // finally change the status bar color if you want to keep it
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            Window window = this.getWindow();
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.deep_purple));
+        }
 
-        webview.setWebViewClient(new WebViewClient() {
-            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                view.loadUrl(request.getUrl().toString());
-                return false;
-            }
-        });
+        //Set up HTML5 compatible webview
+        WebView myWebView = (WebView) findViewById(R.id.nanowallet);
+        myWebView.setFocusable(true);
+        myWebView.setFocusableInTouchMode(true);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.getSettings().setPluginState(WebSettings.PluginState.ON);
+        myWebView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+        myWebView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        myWebView.getSettings().setDomStorageEnabled(true);
+        myWebView.getSettings().setDatabaseEnabled(true);
+        myWebView.getSettings().setAppCacheEnabled(true);
+        myWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("https://www.nanowallet.io");
+
     }
 }
